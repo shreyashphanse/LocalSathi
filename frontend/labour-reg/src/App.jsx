c736793d5b1159790fb3d978f54fb26fb1c0218a
shreyash-demo <shreyashphanse22@gmail.com>
2025-11-12 18:44:19 +0530

    profile screens done

--- frontend/labour-reg/src/App.jsx
+++ frontend/labour-reg/src/App.jsx
@@ -3,6 +3,8 @@ import StartScreen from "./screens/start";
 import LabourRegi from "./screens/labour_regi";
 import ClientRegi from "./screens/client_regi";
 import Login from "./screens/login";
+import LabourProfileTop from "./screens/labour_profile";
+import ClientProfileTop from "./screens/client_profile";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
       <Route path="/labour" element={<LabourRegi />} />
       <Route path="/client" element={<ClientRegi />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/labourprofile" element={<LabourProfileTop />} />
+      <Route path="/clientprofile" element={<ClientProfileTop />} />
     </Routes>
   );
 }
